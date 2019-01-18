ac7edc0267511dcc4b4e3e95e580ced34ae76200
hite <iamhite@gmail.com>
2019-01-18 16:43:34 +0800

    Initial Commit

---
+++ IOSEventTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IOSEventTest
+//
+//  Created by liang on 2019/1/18.
+//  Copyright © 2019 liang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IOSEventTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IOSEventTest
+//
+//  Created by liang on 2019/1/18.
+//  Copyright © 2019 liang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
